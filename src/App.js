04c95eb66478f64d81c9fab62a1065422e8025a7
bogdankhamelyuk <71139952+bogdankhamelyuk@users.noreply.github.com>
2024-03-11 19:38:17 +0100

    ....

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
 import "./App.css";
+import { Flex, Input, Button } from "antd";
+import { useState } from "react";
 
+const inputText = {
+  width: "30vh",
+};
 function App() {
-  return <div></div>;
+  const [newTask, setNewTask] = useState("");
+  const [buttonState, setButtonState] = useState(newTask.length === 0);
+  const [tasks, setTasks] = useState([""]);
+
+  const textChange = (text) => {
+    setNewTask(text);
+    setButtonState(text.length === 0);
+  };
+
+  const buttonClick = () => {
+    const updatedTasks = [...tasks, newTask];
+    setTasks(updatedTasks);
+    setNewTask("");
+    setButtonState(newTask.length === 0);
+  };
+
+  return (
+    <div className="vert-container">
+      <div className="horiz-container">
+        <Input
+          style={inputText}
+          value={newTask}
+          placeholder="Type your task"
+          allowClear
+          onChange={(e) => textChange(e.target.value)}
+        />
+        <Button type="primary" disabled={buttonState} onClick={buttonClick}>
+          Add to the list
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
