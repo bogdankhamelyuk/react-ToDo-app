bfeeba54f336c380c564e89dde16dd4ec51fb322
bogdankhamelyuk <71139952+bogdankhamelyuk@users.noreply.github.com>
2024-03-12 11:27:32 +0100

    better layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import "./App.css";
 import { List, Input, Button, Checkbox } from "antd";
-import { useEffect, useState } from "react";
-import { unselectItems } from "./Utils";
-const inputText = {
-  width: "30vh",
-};
-const taskList = {
-  maxHeight: "55vh",
-  minHeight: "55vh",
-  overflowY: "auto",
-  border: "none",
-  width: "50vh",
-  marginTop: "1vh",
-};
+import { useState } from "react";
+import { unselectItems, inputText, taskList, listItem } from "./Utils";
+import { DeleteOutlined } from "@ant-design/icons";
 
 function App() {
   const [text, setText] = useState([]);
-
   const [tasks, setTasks] = useState([]);
   const [selectedTasks, setSelectedTasks] = useState([]);
 
@@ -39,47 +28,47 @@ function App() {
     setText([]);
   };
 
+  const taskEdit = (index, newText) => {
+    const updatedTasks = [...tasks];
+    updatedTasks[index] = newText;
+    setTasks(updatedTasks);
+  };
+
   return (
     <div className="page-container">
       <div className="list-header">Your daily tasks</div>
+      <div
+        className="horiz-container"
+        style={{
+          justifyContent: "flex-end",
+          alignSelf: "stretch",
+        }}
+      >
+        <Button danger type="primary" icon={<DeleteOutlined />}></Button>
+      </div>
       <List
         style={taskList}
         bordered
         dataSource={tasks}
         renderItem={(item, index) => (
-          <List.Item>
+          <List.Item style={listItem}>
             <Checkbox
               onChange={(e) => {
                 const isChecked = e.target.checked;
                 handleSelectionChange(index, isChecked);
               }}
-            >
-              {item}
-            </Checkbox>
+              style={{ marginRight: "1vh" }}
+            ></Checkbox>
+            <Input value={item} variant="borderless" onChange={(e) => taskEdit(index, e.target.value)} />
           </List.Item>
         )}
       />
-      {/* here goes input, add button and delete button underneath  */}
       <div className="vert-container">
+        {/*  INPUT and ADD Button */}
         <div className="horiz-container">
-          <Input
-            style={inputText}
-            value={text}
-            placeholder="Type your task"
-            allowClear
-            onChange={(e) => textChange(e.target.value)}
-          />
-          <Button
-            type="primary"
-            disabled={text.length === 0}
-            onClick={buttonClick}
-          >
+          <Input style={inputText} value={text} placeholder="Type your task" allowClear onChange={(e) => textChange(e.target.value)} />
+          <Button type="primary" disabled={text.length === 0} onClick={buttonClick}>
             Add to the list
-          </Button>
-        </div>
-        <div className="horiz-container">
-          <Button type="primary" danger disabled={selectedTasks.length === 0}>
-            Delete
           </Button>
         </div>
       </div>
--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,20 @@
 export const unselectItems = (tasks, selectedItems, index) => {
   return selectedItems.filter((item) => item !== tasks[index]);
 };
+
+export const inputText = {
+  width: "30vh",
+};
+export const taskList = {
+  maxHeight: "55vh",
+  minHeight: "55vh",
+  overflowY: "auto",
+  border: "none",
+  width: "50vh",
+  marginTop: "1vh",
+};
+
+export const listItem = {
+  display: "flex",
+  justifyContent: "flex-start",
+};
