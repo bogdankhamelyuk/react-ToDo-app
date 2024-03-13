eefac64125b77c4e20966aa1193efe3f0ab4aaaa
bogdankhamelyuk <71139952+bogdankhamelyuk@users.noreply.github.com>
2024-03-13 20:41:46 +0100

    trying w CI

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "reactToDoapp": {
-      "GraphQLAPIEndpointOutput": "string",
-      "GraphQLAPIIdOutput": "string",
-      "GraphQLAPIKeyOutput": "string"
-    }
-  }
-}
