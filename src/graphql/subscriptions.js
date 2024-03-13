3de643b3443df842c1ea92b8089c46b3e1219880
bogdankhamelyuk <71139952+bogdankhamelyuk@users.noreply.github.com>
2024-03-13 19:07:51 +0100

    Revert "deleted evth w awsw"
    This reverts commit 646462cf3e9613fdab3ab7892f4c942eac4f0f63.

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,36 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateToDo = /* GraphQL */ `
+  subscription OnCreateToDo($filter: ModelSubscriptionToDoFilterInput) {
+    onCreateToDo(filter: $filter) {
+      id
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onUpdateToDo = /* GraphQL */ `
+  subscription OnUpdateToDo($filter: ModelSubscriptionToDoFilterInput) {
+    onUpdateToDo(filter: $filter) {
+      id
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const onDeleteToDo = /* GraphQL */ `
+  subscription OnDeleteToDo($filter: ModelSubscriptionToDoFilterInput) {
+    onDeleteToDo(filter: $filter) {
+      id
+      description
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
