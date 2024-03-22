a6c04c2a0449642ce33c785026f61f1ffe324d4e
bogdankhamelyuk <71139952+bogdankhamelyuk@users.noreply.github.com>
2024-03-22 19:24:13 +0100

    deleted non important parts outta express

--- aws-node-express-api-project/index.js
+++ aws-node-express-api-project/index.js
@@ -1,8 +1,5 @@
 const serverless = require("serverless-http");
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const mongoose = require("mongoose");
-require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
@@ -14,57 +11,6 @@ app.get("/", (req, res, next) => {
   return res.status(200).json({
     message: "Hello from root!",
   });
-});
-
-const uri = `mongodb+srv://bodya01990:jHqaxB2VGi9rSXuL@serverlessinstance0.rd5xfm9.mongodb.net/?retryWrites=true&w=majority&appName=ServerlessInstance0`;
-// mongodb+srv://bodya01990:jHqaxB2VGi9rSXuL@serverlessinstance0.rd5xfm9.mongodb.net/?retryWrites=true&w=majority&appName=ServerlessInstance0
-
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB", err);
-  });
-
-// Define schema
-const taskSchema = new mongoose.Schema({
-  uid: String,
-  tasks: [String],
-  selectedTasks: [String],
-});
-
-// Create model
-const Task = mongoose.model("Task", taskSchema);
-
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// Route to handle POST requests
-app.post("/api/tasks", async (req, res) => {
-  const { uid, tasks, selectedTasks } = req.body;
-  try {
-    const existingUser = await Task.findOne({ uid: uid });
-    if (existingUser) {
-      // If the user exists, update their data
-      existingUser.tasks = tasks;
-      existingUser.selectedTasks = selectedTasks;
-      await existingUser.save();
-      res.status(200).json({ message: { uid, tasks, selectedTasks } });
-    } else {
-      // If the user doesn't exist, create a new user document
-      const newUser = new Task({ uid, tasks, selectedTasks });
-      await newUser.save();
-      res.status(201).json({ message: "User created successfully" });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal server error" });
-  }
 });
 
 app.get("/api/firebase-config", (req, res) => {
@@ -79,11 +25,5 @@ app.get("/api/firebase-config", (req, res) => {
   };
   res.json(firebaseConfig);
 });
-
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
 
 module.exports.handler = serverless(app);
